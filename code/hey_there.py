7aae0b60de349ecdc43bb3d64eb32968498864c8
Oleg Serikov <olegserikov@mail.ru>
2019-11-13 08:03:51 +0300

    initial commit

---
+++ code/hey_there.py
@@ -0,0 +1,16 @@
+
+# todo:
+# 1. open read and close any file in the repo
+#
+# 2. count the number of lines in each file in the repo
+#
+# 3. read any word from keyboard
+#    for each file in the repo
+#    count the number of times this word occurs in the file
+#    output filenameSPACEwordSPACEoccurrences_number
+#
+# 4. who is more selfish: Rick or Morty
+#
+# 5. find the most frequent word in a file
+
+# YOUR CODE BELOW
